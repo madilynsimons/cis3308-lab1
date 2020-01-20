0a8733acb34b6d896564cfaf501486e813388f33
Madilyn Simons <madilynsimons@gmail.com>
2020-01-20 02:33:36 -0500

    Added comments to code

--- web/js/dropdownFw.js
+++ web/js/dropdownFw.js
@@ -1,18 +1,22 @@
 "use strict";
 
-function dropdownFw(dropHeaderStyle, dropContentStyle, hiddenRight){
+function dropdownFw(dropHeaderStyle, dropContentStyle){
     
+    // drop down headers
     var headerList = document.getElementsByClassName(dropHeaderStyle);
     
+    // add onclick functions to drop down headers
     for(var i = 0; i < headerList.length; i++){
         
         headerList[i].onclick = function () {
+            // get dropcontent
             var dContent = this.parentElement.getElementsByClassName(dropContentStyle)[0];
-            hideExcept(dContent);
-            toggle(dContent);
+            hideExcept(dContent); // close if opened
+            toggle(dContent); // hide or show submenu
         }; // end of onclick
     } // end of for loop
     
+    // hide or show dropcontent
     function toggle(ele){
         if(ele.style.visibility == "visible"){
             hide(ele);
@@ -21,16 +25,17 @@ function dropdownFw(dropHeaderStyle, dropContentStyle, hiddenRight){
         }
     } // end of toggle
     
+    // hide element
     function hide(ele){
-        ele.style.right = hiddenRight;
         ele.style.visibility = "hidden";
     } // end of hide
     
+    // show element
     function show(ele){
         ele.style.visibility = "visible";
-        ele.style.right = "0px";
     } // end of show
     
+    // hide all elements except given element
     function hideExcept(ele){
         var dropContentList = document.getElementsByClassName(dropContentStyle);
         for(var i = 0; i < dropContentList.length; i++){
@@ -40,6 +45,7 @@ function dropdownFw(dropHeaderStyle, dropContentStyle, hiddenRight){
         } // end of for loop
     } // end of hideExcept
     
+    // close dropcontents if another dropcontent is clicked
     window.onclick = function (event) {
         
         if(!event.target.className.includes(dropHeaderStyle)){
