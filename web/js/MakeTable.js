d4ad30908e8d8d4d37ae7751dfbfcbd75b58260a
Madilyn Simons <madilynsimons@gmail.com>
2020-02-02 23:33:31 -0500

    Added user-input filter to MakeTable.js

--- web/js/MakeTable.js
+++ web/js/MakeTable.js
@@ -1,4 +1,56 @@
 function MakeTable(list, id, sortOrderPropName) {
+    
+    // return true if any property of obj contains searchKey. Else return false.
+    function isToShow(obj, searchKey) {
+        if (!searchKey || searchKey.length === 0) {
+            return true; // show the object if searchKey is empty
+        }
+        var searchKeyUpper = searchKey.toUpperCase();
+        for (var prop in obj) {
+            var propVal = obj[prop]; // associative array, using property name as if index. 
+            console.log("checking if " + searchKeyUpper + " is in " + propVal);
+            var propValUpper = propVal.toUpperCase();
+            if (propValUpper.includes(searchKeyUpper)) {
+                console.log("yes it is inside");
+                return true;
+            }
+        }
+        console.log("no it is not inside");
+        return false;
+    } // isToShow 
+    
+    function addDataRows(filterValue) {
+
+        // remove old tbody element if there is one (else you'll get sorted rows added to end of what's there).
+        var oldBody = newTable.getElementsByTagName("tbody");
+        if (oldBody[0]) {
+            console.log("ready to remove oldBody");
+            newTable.removeChild(oldBody[0]);
+        }
+
+        // Add one row (to HTML table) per element in the array.
+        // Each array element has a list of properties that will become 
+        // td elements (Table Data, a cell) in the HTML table. 
+        var tableBody = document.createElement("tbody");
+        newTable.appendChild(tableBody);
+        for (var i in list) {
+            if (isToShow(list[i], filterValue)) {
+                console.log("adding row " + i + " to the HTML table");
+
+                var tableRow = document.createElement("tr");
+                tableBody.appendChild(tableRow);
+
+                // create one table data <td> content matching the property name
+                var obj = list[i];
+                for (var prop in obj) {
+                    addToRow("td", tableRow, obj[prop], alignment(obj[prop]));
+                }
+
+            } else {
+                console.log("not adding row " + i + " to the HTML table");
+            }
+        } // for loop 
+    } // addDataRows
 
     function jsSort(list, byProperty) {
 
@@ -91,6 +143,12 @@ function MakeTable(list, id, sortOrderPropName) {
 
     console.log("function MakeSortableTable called sorting by " + sortOrderPropName);
     jsSort(list, sortOrderPropName);
+    
+    document.getElementById(id).innerHTML = "Filter by: "; // blank out the target DOM object
+    
+        // Create a filter text box for user input and append it.
+    var searchInput = document.createElement("input");
+    document.getElementById(id).appendChild(searchInput);
 
     // Create a new HTML table (DOM object)
     var newTable = document.createElement("table");
@@ -126,9 +184,17 @@ function MakeTable(list, id, sortOrderPropName) {
             addToRow("td", tableRow, obj[prop], alignment(obj[prop]));
         }
     }
+    
+    // Initially searchInput.value should be "" and when passing that to 
+    // function isToShow should always return meaning all rows will initially show. 
+    addDataRows(searchInput.value);
+    
+    searchInput.onkeyup = function () {
+        console.log("search filter changed to " + searchInput.value);
+        addDataRows(searchInput.value);
+    };
 
     // The DOM object (htmlTable) will not show unless you attach it somewhere on the page.
-    document.getElementById(id).innerHTML = ""; // blank out the target DOM object
     document.getElementById(id).appendChild(newTable);
 
 }  // MakeTable
