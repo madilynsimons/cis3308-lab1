d91c3c722377d510046e6ed2e3f815711a1bb61c
Madilyn Simons <madilynsimons@gmail.com>
2020-04-12 22:29:12 -0400

    parks.insert

--- web/js/components/parks.js
+++ web/js/components/parks.js
@@ -1,36 +1,417 @@
-function parks(id){
-    var content = `
-        <div id="parks_table" class="clickSort"></div>
-    `;
-    
-    document.getElementById(id).innerHTML = content;
-    
-    function formatCurrency(num) {
-        var myNum = Number(num);
-        return myNum.toLocaleString("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2});
-    }
-    
-    ajax("webAPIs/listParksAPI.jsp", processData, "parks_table");
-    
-    function processData(obj) {
+var parks = {};
 
-        console.log(obj);
-        
-        if(obj.dbError.length > 0){
-            document.getElementById("parks_table").innerHTML = obj.dbError;
-            return;
+(function () {  // This is an IIFE, an immediately executing (anonymous) function
+    //alert("I am an IIFE!");
+    parks.list = function (targetId) {
+
+        // clear out whatever may be currently in the content area
+        var contentDOM = document.getElementById(targetId);
+        contentDOM.innerHTML = "";
+
+        // Remember: getting a successful ajax call does not mean you got data.
+        // There could have been a DB error (like DB unavailable).
+        ajax2({
+            url: "webAPIs/listParksAPI.jsp",
+            successFn: success,
+            errorId: targetId
+        });
+
+        function success(obj) {
+
+            // var obj = JSON.parse(hreq.responseText); // this already done by function ajax2...
+            console.log(obj);
+
+            if (obj.dbError.length > 0) {
+                contentDOM.innerHTML += "Database Error Encountered: " + obj.dbError;
+                return;
+            }
+
+            // Want the User List UI (plus headings and search filter) all to be centered.
+            // Cannot be sure content area will be like this, so create a div inside of the
+            // content area and set the div to be aligned center (HTML table will be styled
+            // margin: auto to make it centered as well).
+            var div = Utils.make({
+                htmlTag: "div",
+                parent: contentDOM
+            });
+            div.style.textAlign = "center";
+
+            var heading = Utils.make({
+                htmlTag: "h2",
+                parent: div
+            });
+
+            Utils.make({// don't need reference to this span tag...
+                htmlTag: "span",
+                innerHTML: "Park List ",
+                parent: heading
+            });
+
+            var img = Utils.make({
+                htmlTag: "img",
+                parent: heading
+            });
+            img.src = CRUD_icons.insert;
+            img.onclick = function () { // you cant pass input params directly into an event handler
+
+                // Originally I had this line of code here:
+                //     users.insertUI(targetId);
+                // And that worked (insert UI displayed and save worked), BUT, afterwards, if you tried to re-run
+                // the user list, nothing would happen -- because this would cause no change in the
+                // browser's address bar (the window.location.hash).
+                //
+                // The solution here is to invoke the user insert UI through a routing rule (since we
+                // happen to have "user register" that can be directly invoked).
+                // For "other" insert (even though you probably won't have a Nav Bar link for inserting "other",
+                // you may need to create a routing rule and invoke that similarly (from the "other" list UI).
+                window.location.hash = "#/parkInsert";
+            };
+
+            Utils.make({
+                htmlTag: "span",
+                innerHTML: " Search Filter: ",
+                parent: div
+            });
+
+            var searchBox = Utils.make({
+                htmlTag: "input",
+                parent: div
+            });
+            searchBox.type = "text";
+            //searchBox.setAttribute("type", "text");  // same thing...
+
+            var deleteErrorMsg = Utils.make({
+                htmlTag: "div",
+                innerHTML: "",
+                parent: div
+            });
+            deleteErrorMsg.id = "deleteErrorMsgId";
+
+            var tableDiv = Utils.make({
+                htmlTag: "div",
+                parent: div
+            });
+
+            // create userList (new array of objects) to have only the desired properties of obj.webUserList.
+            // Add the properties in the order you want them to appear in the HTML table.
+            var parkList = [];
+            console.log("Traversing obj.parkList");
+            for (var i = 0; i < obj.parkList.length; i++) {
+                console.log(i);
+                parkList[i] = {}; // add new empty object to array
+
+                parkList[i].name = obj.parkList[i].name;
+                parkList[i].image = obj.parkList[i].image;
+                parkList[i].description = obj.parkList[i].description;
+                parkList[i].rating = obj.parkList[i].rating;
+                parkList[i].cost = obj.parkList[i].cost;
+                parkList[i].parkId = obj.parkList[i].parkId;
+                parkList[i].webUserId = obj.parkList[i].webUserId;
+                parkList[i].webUserImage = obj.parkList[i].webUserImage;
+                parkList[i].webUserEmail = obj.parkList[i].webUserEmail;
+
+                // Remove this once you are done debugging...
+                parkList[i].errorMsg = obj.parkList[i].errorMsg;
+
+                // *** NEW: ADD EXTRA COLUMN TO DELETE THE RECORD
+                // Note: this needs the word "icon" somewhere in userList[i].delete. Otherwise, the alignTableData function
+                // of TableBuilder will try to turn the delete column (aleady an <img> tag complete with onclick function)
+                // into an <img> tag.
+
+                // here we have single quote around the onclick function call.
+                // Inside the single quote we need to put quotes around the value of targetId (which is content).
+                // For this, we use the back tick and it works.
+                parkList[i].update = "<img src='" + CRUD_icons.update + "' alt='update icon' onclick='parks.updateUI(" +
+                        parkList[i].parkId + ", `" + targetId + "` )' />";
+                parkList[i].delete = "<img src='" + CRUD_icons.delete + "' alt='delete icon' onclick='parks.delete(" +
+                        parkList[i].parkId + ",this)'  />";
+            }
+
+            // add click sortable HTML table to the content area
+
+            // ********************** function tableBuilder.build ***********************************
+            // params.list: an array of objects that are to be built into an HTML table.
+            // params.target: reference to DOM object where HTML table is to be placed (by buildTable) --
+            //         (this is not the id string but actual reference like you get from method getElementById()
+            // params.style: will be added as className to DOM element target,
+            // params.orderPropName (string): name of property (of objects in list) for iniial sort
+            // params.reverse (boolean): if true, initial sort will be high to low (else low to high).
+            // params.imgWidth: any columns that hold image files will be turned into <img> tags with this width.
+
+            tableBuilder.build({
+                list: parkList,
+                target: tableDiv,
+                style: "clickSort",
+                orderPropName: "name",
+                searchKeyElem: searchBox,
+                reverse: false,
+                imgWidth: "50px"
+            });
+        } // end of function success
+    }; // end of function parks.list
+
+    // this code called by insertUI and updateUI -- shared common code.
+    function createInsertUpdateArea (isUpdate, targetId) {
+
+        // set variables as if it will be insert...
+        var webUserIdRowStyle = ' style="display:none" '; // hide row with webUserId
+        var saveFn = "parks.insertSave()";
+
+        // change variables for update
+        if (isUpdate) {
+            webUserIdRowStyle = ""; // don't hide row with webUserId
+            saveFn = "parks.updateSave()";
         }
-        
-        var parksList = obj.parkList;
 
-        // modifications for the image and the membership fee
-        for (var i = 0; i < parksList.length; i++) {
-            parksList[i].image = "<img  src='" + parksList[i].image + "'>";
-            parksList[i].webUserImage = "<img  src='" + parksList[i].webUserImage + "'>";
-            parksList[i].cost = formatCurrency(parksList[i].cost.replace("$","").replace(",",""));
+        var html = `
+            <div id="insertArea">
+                <div id="ajaxError">&nbsp;</div>
+                <table>
+                    <tr ${webUserIdRowStyle}>
+                        <td>Web User Id</td>
+                        <td><input type="text"  id="webUserId" disabled /></td>
+                        <td id="webUserIdError" class="error"></td>
+                    </tr>
+                    <tr>
+                        <td>Email Address</td>
+                        <td><input type="text"  id="userEmail" /></td>
+                        <td id="userEmailError" class="error"></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password"  id="userPassword" /></td>
+                        <td id="userPasswordError" class="error"></td>
+                    </tr>
+                    <tr>
+                        <td>Retype Your Password</td>
+                        <td><input type="password" id="userPassword2" /></td>
+                        <td id="userPassword2Error" class="error"></td>
+                    </tr>
+                    <tr>
+                        <td>Birthday</td>
+                        <td><input type="text" id="birthday" /></td>
+                        <td id="birthdayError" class="error"></td>
+                    </tr>
+                    <tr>
+                        <td>Membership Fee</td>
+                        <td><input type="text" id="membershipFee" /></td>
+                        <td id="membershipFeeError" class="error"></td>
+                    </tr>
+                    <tr>
+                        <td>User Role</td>
+                        <td>
+                            <select id="rolePickList">
+                            <!-- JS code will make ajax call to get all the roles
+                            then populate this select tag's options with those roles -->
+                            </select>
+                        </td>
+                        <td id="userRoleIdError" class="error"></td>
+                    </tr>
+                    <tr>
+                        <td><button onclick="${saveFn}">Save</button></td>
+                        <td id="recordError" class="error"></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        `;
+
+        document.getElementById(targetId).innerHTML = html;
+    }
+
+    parks.updateUI = function (webUserId, targetId) {
+        createInsertUpdateArea(true, targetId); // first param is isUpdate (boolean)
+        ajax2({
+            url: "webAPIs/getUserWithRolesAPI.jsp?id=" + webUserId,
+            successFn: proceed,
+            errorId: "ajaxError"
+        });
+        function proceed(obj) { // obj is what got JSON.parsed from Web API's output
+            dbDataToUI(obj);
+        }
+    };
+
+    parks.insertUI = function (targetId) {
+
+        createInsertUpdateArea(false, targetId); // first param is isUpdate (boolean)
+
+        ajax2({
+            url: "webAPIs/getRolesAPI.jsp",
+            successFn: setRolePickList,
+            errorId: "userRoleIdError"
+        });
+
+        function setRolePickList(jsonObj) {
+
+            console.log("setRolePickList was called, see next line for object holding list of roles");
+            console.log(jsonObj);
+
+            if (jsonObj.dbError.length > 0) {
+                document.getElementById("userRoleIdError").innerHTML = jsonObj.dbError;
+                return;
+            }
+
+            Utils.makePickList({
+                id: "rolePickList", // id of select tag on the page
+                list: jsonObj.roleList, // JS array that holds the objects to populate the select tag
+                valueProp: "userRoleType", // field name of objects in list that holds the values of the select tag options
+                keyProp: "userRoleId"      // field name of objects in list that holds the keys of the options
+            });
+
+        } // setRolePickList
+
+    }; // parks.insertUI
+
+
+    function dbDataToUI(obj) {
+
+        var webUserObj = obj.webUser;
+        var roleList = obj.roleInfo.roleList;
+
+        document.getElementById("webUserId").value = webUserObj.webUserId;
+        document.getElementById("userEmail").value = webUserObj.userEmail;
+        document.getElementById("userPassword").value = webUserObj.userPassword;
+        document.getElementById("userPassword2").value = webUserObj.userPassword;
+        document.getElementById("birthday").value = webUserObj.birthday;
+        document.getElementById("membershipFee").value = webUserObj.membershipFee;
+        console.log("selected role id is " + webUserObj.userRoleId);
+        Utils.makePickList({
+            id: "rolePickList", // id of <select> tag in UI
+            list: roleList, // JS array that holds objects to populate the select list
+            valueProp: "userRoleType", // field name of objects in list that hold the values of the options
+            keyProp: "userRoleId", // field name of objects in list that hold the keys of the options
+            selectedKey: webUserObj.userRoleId  // key that is to be pre-selected (optional)
+        });
+    }
+
+    // a private function
+    function getUserDataFromUI() {
+
+        // New code for handling role pick list.
+        var ddList = document.getElementById("rolePickList");
+
+        // create a user object from the values that the user has typed into the page.
+        var membershipFee = document.getElementById("membershipFee").value;
+        if(membershipFee){
+            if(membershipFee.length > 0){
+                if(membershipFee.charAt(0) == "$"){
+                    membershipFee = membershipFee.substring(1);
+                }
+            }
         }
 
-        MakeTable(parksList, "parks_table", "parkId"); // iniritally sort by webUserId
+        var birthday = document.getElementById("birthday").value;
+        if(birthday){
+            var birthdayParsed = Date.parse(birthday);
+            if(!isNaN(birthdayParsed)){
+                var birthdayDate = new Date(birthday);
+                var month = (birthdayDate.getMonth()+1).toString();
+                var date = birthdayDate.getDate().toString();
+                var year = birthdayDate.getFullYear().toString();
+
+                if(month.length == 1){
+                    month = "0" + month;
+                }
+                if(date.length == 1){
+                    date = "0" + date;
+                }
+                birthday = month + "/" + date + "/" + year;
+            }
+        }
+
+        var userInputObj = {
+            "webUserId": document.getElementById("webUserId").value,
+            "userEmail": document.getElementById("userEmail").value,
+            "userPassword": document.getElementById("userPassword").value,
+            "userPassword2": document.getElementById("userPassword2").value,
+            "birthday": birthday,
+            "membershipFee": membershipFee,
+
+            // Modification here for role pick list
+            //"userRoleId": document.getElementById("userRoleId").value,
+            "userRoleId": ddList.options[ddList.selectedIndex].value,
+
+            "userRoleType": "",
+            "errorMsg": ""
+        };
+
+        console.log(userInputObj);
+
+        // JSON.stringify converts the javaScript object into JSON format
+        // (the reverse operation of what gson does on the server side).
+        //
+        // Then, you have to encode the user's data (encodes special characters
+        // like space to %20 so the server will accept it with no security error.
+        return encodeURIComponent(JSON.stringify(userInputObj));
+        //return escape(JSON.stringify(userInputObj));
     }
-   
-}
+
+    function writeErrorObjToUI(jsonObj) {
+        console.log("here is JSON object (holds error messages.");
+        console.log(jsonObj);
+
+        document.getElementById("userEmailError").innerHTML = jsonObj.userEmail;
+        document.getElementById("userPasswordError").innerHTML = jsonObj.userPassword;
+        document.getElementById("userPassword2Error").innerHTML = jsonObj.userPassword2;
+        document.getElementById("birthdayError").innerHTML = jsonObj.birthday;
+        document.getElementById("membershipFeeError").innerHTML = jsonObj.membershipFee;
+        document.getElementById("userRoleIdError").innerHTML = jsonObj.userRoleId;
+        document.getElementById("recordError").innerHTML = jsonObj.errorMsg;
+    }
+
+    parks.insertSave = function () {
+
+        console.log("parks.insertSave was called");
+
+        // create a user object from the values that the user has typed into the page.
+        var myData = getUserDataFromUI();
+
+        ajax2({
+            url: "webAPIs/insertUserAPI.jsp?jsonData=" + myData,
+            successFn: processInsert,
+            errorId: "recordError"
+        });
+
+        function processInsert(jsonObj) {
+
+            // the server prints out a JSON string of an object that holds field level error
+            // messages. The error message object (conveniently) has its fiels named exactly
+            // the same as the input data was named.
+
+            if (jsonObj.errorMsg.length === 0) { // success
+                jsonObj.errorMsg = "Record successfully inserted !!!";
+            }
+
+            writeErrorObjToUI(jsonObj);
+        }
+    };
+
+    parks.updateSave = function () {
+
+     console.log("parks.updateSave was called");
+
+        // create a user object from the values that the user has typed into the page.
+        var myData = getUserDataFromUI();
+
+        ajax2({
+            url: "webAPIs/updateUserAPI.jsp?jsonData=" + myData,
+            successFn: processInsert,
+            errorId: "recordError"
+        });
+
+        function processInsert(jsonObj) {
+
+            // the server prints out a JSON string of an object that holds field level error
+            // messages. The error message object (conveniently) has its fiels named exactly
+            // the same as the input data was named.
+
+            if (jsonObj.errorMsg.length === 0) { // success
+                jsonObj.errorMsg = "Record successfully updated !!!";
+            }
+
+            writeErrorObjToUI(jsonObj);
+        }
+
+    };
+
+}());  // the end of the IIFE
